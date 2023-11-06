ab5218366b92d25a3000458b5623c7455fba2400
Azizbek G'ulomov <azizbekgulomovweb@gmail>
2023-11-06 22:55:47 +0500

    js kodlar qo'shildi

---
+++ script.js
@@ -0,0 +1,28 @@
+// window.addEventListener("scroll", function () {
+//     var nav = document.querySelector("nav");
+//     if (window.scrollY > 80) {
+//         nav.style.position = "fixed";
+//         nav.style.top = "0px";
+//     } else {
+//         nav.style.position = "absolute";
+//         nav.style.top = "80px"; 
+//     }
+// });
+
+// ________________________________________________________________________________________________________________________________
+
+const contact_us_x = document.querySelector(".contact-us-x");
+const contact_us = document.querySelector(".contact-us");
+const contact = document.querySelectorAll(".contact");
+
+contact_us.style.display = "none";
+
+contact_us_x.addEventListener("click", function () {
+    contact_us.style.display = "none";
+});
+
+contact.forEach(function (item) {
+    item.addEventListener("click", function () {
+        contact_us.style.display = "block"; 
+    });
+});
